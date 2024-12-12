d316ce295428e76d8b3b2c916ff5182fff151a54
FukuiSho <hezhedemo@gmail.com>
2024-12-12 15:32:20 +0900

    仮のホーム画面の作成。

--- dev/mobile/app/(tabs)/index.tsx
+++ dev/mobile/app/(tabs)/index.tsx
@@ -1,77 +1,34 @@
-import { Image, StyleSheet, Platform } from 'react-native';
+import React from 'react';
+import { Link, Stack } from 'expo-router';
+import { SafeAreaView, StyleSheet, View, Text, useColorScheme } from 'react-native';
+// import { NavigationContainer } from '@react-navigation/native';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 
-import { HelloWave } from '@/components/HelloWave';
-import ParallaxScrollView from '@/components/ParallaxScrollView';
-import { ThemedText } from '@/components/ThemedText';
-import { ThemedView } from '@/components/ThemedView';
+import HomeScreen from '@/components/screens/HomeScreen';
+import SettingsScreen from '@/components/screens/SettingScreen';
 
-export default function HomeScreen() {
+const Tab = createBottomTabNavigator();
+
+const App = () => {
+  const isDarkMode = useColorScheme() === 'dark';
+
   return (
-    <ParallaxScrollView
-      headerBackgroundColor={{ light: '#A1CEDC', dark: '#1D3D47' }}
-      headerImage={
-        <Image
-          source={require('@/assets/images/ChatGPT.jpg')}
-          style={styles.reactLogo}
-        />
-      }>
 
-      <ThemedView style={styles.titleContainer}>
-        <ThemedText type="title">Kodomori!
-        </ThemedText>
-        <HelloWave />
-      </ThemedView>
-      <ThemedView style={styles.stepContainer}>
-        <ThemedText type="subtitle">Step 1: Try it</ThemedText>
-        <ThemedText>
-          Edit <ThemedText type="defaultSemiBold">app/(tabs)/index.tsx</ThemedText> to see changes.
-          Press{' '}
-          <ThemedText type="defaultSemiBold">
-            {Platform.select({
-              ios: 'cmd + d',
-              android: 'cmd + m',
-              web: 'F12'
-            })}
-          </ThemedText>{' '}
-          to open developer tools.
-        </ThemedText>
-      </ThemedView>
-      <ThemedView style={styles.stepContainer}>
-        <ThemedText type="subtitle">Step 2: Explore</ThemedText>
-        <ThemedText>
-          Tap the Explore tab to learn more about what's included in this starter app.
-        </ThemedText>
-      </ThemedView>
-      <ThemedView style={styles.stepContainer}>
-        <ThemedText type="subtitle">Step 3: Get a fresh start</ThemedText>
-        <ThemedText>
-          When you're ready, run{' '}
-          <ThemedText type="defaultSemiBold">npm run reset-project</ThemedText> to get a fresh{' '}
-          <ThemedText type="defaultSemiBold">app</ThemedText> directory. This will move the current{' '}
-          <ThemedText type="defaultSemiBold">app</ThemedText> to{' '}
-          <ThemedText type="defaultSemiBold">app-example</ThemedText>.
-        </ThemedText>
-      </ThemedView>
-    </ParallaxScrollView>
+      <SafeAreaView style={styles.safeArea}>
+        <Tab.Navigator>
+          <Tab.Screen name="Home" component={HomeScreen} />
+          <Tab.Screen name="Settings" component={SettingsScreen} />
+        </Tab.Navigator>
+      </SafeAreaView>
+
   );
-}
+};
 
 const styles = StyleSheet.create({
-  titleContainer: {
-    flexDirection: 'row',
-    alignItems: 'center',
-    gap: 8,
-  },
-  stepContainer: {
-    gap: 8,
-    marginBottom: 8,
-  },
-  reactLogo: {
-    height: 200,
-    width: 800,
-    bottom: '50%',
-    left: '50%',
-    transform: 'translate(-50%, 50%)',
-    position: 'absolute',
+  safeArea: {
+    flex: 1,
+    backgroundColor: 'white',
   },
 });
+
+export default App;
